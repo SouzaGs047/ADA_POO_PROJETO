2fb1cc95b8abf6f9104e8b373aca4a8e947632b6
Your Name <you@example.com>
2023-08-18 23:39:04 -0300

    first commit

--- Catalogo_IMDB/src/Diretores.java
+++ Catalogo_IMDB/src/Diretores.java
@@ -1,2 +1,11 @@
 public class Diretores {
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
